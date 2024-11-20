d72394cb5518bc732bb0895ec003f9b0792b1514
abikoushi <a.b.e.k.o.69@gmail.com>
2024-11-20 11:04:32 +0900

    learn obs_prec

--- example/testrun.R
+++ example/testrun.R
@@ -1,38 +1,39 @@
 library(Matrix)
 library(VBspPCA)
-#library(onlinePCA)
+#library(oocPCA)
 ###
 #M<-2
-miris <- scale(as.matrix(iris[,-5]), center = TRUE, scale = FALSE)
+miris <- scale(log(as.matrix(iris[,-5])), center = TRUE, scale = FALSE)
 miris <- as(miris, "TsparseMatrix")
 ca5 <- rgb(0,0,0,0.5)
 col3 <- hcl.colors(3, palette = "Set 2", alpha = 0.9)
 
-out <- VBPCA(miris, rank = 2, iter = 10, prior_prec = 1)
+out <- VBPCA(miris, rank = 2, iter = 10, prior_prec = 0.1, a=1, b=1)
 plot(out$mean_row, col=col3[iris$Species], pch=16)
 plot(out$mean_row%*%t(out$mean_col), as.matrix(miris), col=ca5)
-abline(0,1,col=ca5)
-plot(out$logprob, type="l")
+abline(0, 1, col=ca5)
+plot(out$logprob[-1], type="l")
 
-writeMM(log(miris), "test.mtx")
+sqrt(1/out$obs_prec)
+
+
+writeMM(miris, "test.mtx")
 system.time({
   out_s <- VBspPCA:::SVBPCA("test.mtx", rank = 2,
-                            b_size = 10,
-                            subiter = 10,
-                            n_epochs = 10,
-                            forgetting=0.9, delay=1)
+                            b_size = 100,
+                            subiter = 5,
+                            n_epochs = 500,
+                            prior_prec = 0.01,
+                            prior_shape = 1.1, prior_rate = 1.1,
+                            forgetting=0.5, delay=0.5)
 })
 #forgetting: (0.5, 1]
 #delay: >0
+out$obs_prec
 plot(out_s$logprob, type="l")
-plot(out_s$mean_row%*%t(out_s$mean_col), log(as.matrix(miris)), col=ca5)
+plot(out_s$mean_row%*%t(out_s$mean_col), as.matrix(miris), col=ca5)
 abline(0,1,col=ca5)
-
-out_s$prec_row
-out$prec_row
-
-out_s$prec_col
-out$prec_col
+plot(out_s$mean_row)
 ###
 set_data <- function(L, nrow, ncol, center=0, scale=1){
   Z <- matrix(rnorm(L*nrow,0,scale), nrow, L)
@@ -56,7 +57,7 @@ set_data <- function(L, nrow, ncol, center=0, scale=1){
 set.seed(123); dat <- set_data(2, 199, 205)
 mean(dat$Y==0)
 length(dat$Y)
-Y <- as(dat$Y, "TsparseMatrix")
+Y <- as(log1p(dat$Y), "TsparseMatrix")
 
 writeMM(Y, "test.mtx")
 hist(dat$Y, breaks = "FD")
--- src/vb_norm.cpp
+++ src/vb_norm.cpp
@@ -5,6 +5,18 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
 
+double calc_elbo(const double & R, const double & obs_prec,
+                 const double & ahat, const double & bhat,
+                 const double & a, const double & b){
+  //return -0.5*obs_prec*R + 0.5*(R::digamma(ahat)-log(bhat)) - kl2gamma(ahat, bhat, a, b);
+  return -0.5*obs_prec*R + 0.5*(R::digamma(ahat)-log(bhat)) - kl2gamma(ahat, bhat, a, b);
+}
+
+void up_lambda(double & obs_prec, double & bhat, const double & ahat, const double & R, const double & b){
+  bhat = 0.5*R+b;
+  obs_prec = ahat/bhat;
+}
+
 void up_eta_w(arma::mat & num_w,
               const arma::vec & y,
               const arma::uvec & rowi,
@@ -24,15 +36,15 @@ double up_eta_z(arma::mat & num_z,
               const arma::mat & Z,
               const arma::mat & W) {
   num_z.fill(0.0);
-  double R = 0;
+  double R = 0.0;
   for(int n=0; n<y.n_rows; n++){
-    R += dot(Z.row(rowi(n)),W.row(coli(n)))*y(n);
+    R += -2.0*dot(Z.row(rowi(n)), W.row(coli(n)))*y(n) + pow(y(n),2);
     num_z.row(rowi(n)) += W.row(coli(n))*y(n);
   }
-  return 2.0*R;
+  return R;
 }
 
-double up_theta(arma::mat & Z, 
+void up_theta(arma::mat & Z, 
                 arma::mat & W,
                 arma::mat & prec_z,
                 arma::mat & prec_w,
@@ -51,13 +63,11 @@ double up_theta(arma::mat & Z,
   prec_w = obs_prec*ZZ + prior;
   up_eta_w(num_w, y, rowi, coli, Z);
   W = obs_prec*arma::trans(solve(prec_w, num_w.t(), arma::solve_opts::likely_sympd));
-  
   arma::mat WW = W.t() * W;
   prec_z = obs_prec*WW + prior;
-  R = up_eta_z(num_z,y,rowi,coli, Z, W);
+  R = up_eta_z(num_z, y, rowi, coli, Z, W);
   Z = obs_prec*arma::trans(solve(prec_z, num_z.t(), arma::solve_opts::likely_sympd));
-  R += accu(WW%ZZ); //!!!
-  return obs_prec*R;
+  R += trace(WW*ZZ); //!!
 }
 
 // [[Rcpp::export]]
@@ -79,12 +89,14 @@ List doVB_norm(const arma::vec & y,
   arma::vec logprob = arma::zeros<arma::vec>(iter);
   double N = Nr*Nc; //int to double
   double ahat = 0.5*N+a;
+  double bhat = b;
   double R = 0;
-  const double sumy2 = 0.5*accu(pow(y,2));
+  //const double sumy2 = 0.5*accu(pow(y,2));
   for (int i=0; i<iter; i++) {
-    double lp = up_theta(Z, W, prec_z, prec_w, R, y, rowi, coli, obs_prec, prior_prec);
-    //up_lambda(obs_prec, ahat, sumy2+R+b);
-    logprob(i) = -0.5*obs_prec*(sumy2+R);
+    up_theta(Z, W, prec_z, prec_w, R, y, rowi, coli, obs_prec, prior_prec);
+    up_lambda(obs_prec, bhat, ahat, R, b);
+    Rprintf("%f\n", R);
+    logprob(i) = calc_elbo(R, obs_prec, ahat, bhat, a, b);
   }
   return List::create(Named("mean_row") = Z,
                       Named("mean_col") = W,
@@ -99,7 +111,7 @@ List doVB_norm(const arma::vec & y,
 //mini-batche version
 /////
 
-double up_theta_s(arma::mat & Z, 
+void up_theta_s(arma::mat & Z, 
                 arma::mat & W,
                 arma::mat & prec_z,
                 arma::mat & prec_w,
@@ -115,19 +127,16 @@ double up_theta_s(arma::mat & Z,
   arma::mat num_z(Z.n_rows, Z.n_cols);
   arma::mat num_w(W.n_rows, W.n_cols);
   int L = W.n_cols;
-  
   const arma::mat prior = arma::diagmat(prior_prec*arma::ones<arma::vec>(L)); 
   arma::mat ZZ = Z.t() * Z;
   prec_w = obs_prec*ZZ + prior;
   up_eta_w(num_w, y, rowi, coli, Z);
   W.rows(uid_c) = obs_prec*arma::trans(solve(NS*prec_w, num_w.rows(uid_c).t(), arma::solve_opts::likely_sympd));
-  
   arma::mat WW = W.t() * W;
   prec_z = obs_prec*WW + prior;
   R = up_eta_z(num_z, y, rowi, coli, Z, W);
   Z.rows(uid_r) = obs_prec*arma::trans(solve(NS*prec_z, num_z.rows(uid_r).t(), arma::solve_opts::likely_sympd));
-  R += accu(WW%ZZ); //!!!
-  return obs_prec*R;
+  R += trace(WW*ZZ); //!!
 }
 
 // [[Rcpp::export]]
@@ -150,14 +159,15 @@ List doVB_norm_s(const arma::vec & y,
   arma::vec logprob = arma::zeros<arma::vec>(iter);
   double N = Nr*Nc; //int to double
   double ahat = 0.5*N+a;
+  double bhat = b;
   double R = 0;
   double S = y.n_rows;
   double NS = S/N1;
-  const double sumy2 = 0.5*accu(pow(y,2));
+  //const double sumy2 = accu(pow(y,2));
   for (int i=0; i<iter; i++) {
-    double lp = up_theta_s(Z, W, prec_z, prec_w, R, y, rowi, coli, uid_r, uid_c, obs_prec, prior_prec, NS);
-    //up_lambda(obs_prec, ahat, sumy2+R+b);
-    logprob(i) = -0.5*obs_prec*(sumy2+R);
+    up_theta_s(Z, W, prec_z, prec_w, R, y, rowi, coli, uid_r, uid_c, obs_prec, prior_prec, NS);
+    up_lambda(obs_prec, bhat, ahat, R, b);
+    logprob(i) = calc_elbo(R, obs_prec, ahat, bhat, a, b);
   }
   return List::create(Named("mean_row") = Z,
                       Named("mean_col") = W,
